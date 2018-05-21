ddc14c1498e8dca38735e979921cdd482af2b880
jakub krhovjak <jakub.krhojvak@aevi.cz>
2018-05-21 09:49:39 +0200

    Just playing with WebFlux

--- mongo-target/src/main/java/com/japca/mongo/MongoApplication.java
+++ mongo-target/src/main/java/com/japca/mongo/MongoApplication.java
@@ -3,6 +3,10 @@ package com.japca.mongo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+
+import javax.annotation.PostConstruct;
+
 
 @SpringBootApplication
 public class MongoApplication {
@@ -11,11 +15,16 @@ public class MongoApplication {
 	private MongoDao mongoDao;
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MongoApplication.class, args);
 	}
 
-//	@PostConstruct
-//	public void save() {
-//		mongoDao.save(new Record(1L,"ahoj1", "desription", 23));
-//	}
+	@PostConstruct
+	public void save() {
+		System.out.println();
+		Flux.just("Ahoj", "Ja", "jsem", "tvuj", "prvni", "reactivni", "stream")
+				.log()
+//				.doOnSubscribe(() -> new Event())
+				.subscribe(item -> System.out.println("ahosj"));
+	}
 }
--- mongo-target/src/test/java/com/japca/mongo/MongoApplicationTests.java
+++ mongo-target/src/test/java/com/japca/mongo/MongoApplicationTests.java
@@ -1,16 +1,94 @@
 package com.japca.mongo;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class MongoApplicationTests {
 
-	@Test
-	public void contextLoads() {
+class Event implements Subscription {
+
+	@Override
+	public void request(long n) {
+		System.out.println("Request");
 	}
 
+	@Override
+	public void cancel() {
+		System.out.println("Cancel");
+	}
+}
+
+class MySubscriber implements Subscriber<String> {
+
+	private long count = 0;
+	private Subscription subscription;
+
+
+	@Override
+	public void onSubscribe(Subscription s) {
+		this.subscription = s;
+		subscription.request(2);
+	//	System.out.println("On subscribe");
+	}
+
+	@Override
+	public void onNext(String s) {
+		count++;
+		if (count>=2) {
+			count = 0;
+			subscription.request(2);
+		}
+//		System.out.println("On next");
+	}
+
+	@Override
+	public void onError(Throwable t) {
+	//	System.out.println("On Error");
+	}
+
+	@Override
+	public void onComplete() {
+	//	System.out.println("On Complete");
+	}
+}
+
+
+//@Slf4j
+public class MongoApplicationTests {
+
+	Logger log = LoggerFactory.getLogger(getClass()) ;
+
+	@Test
+	public void FluxTest() throws InterruptedException {
+		Flux.just("Ahoj", "Ja", "jsem", "tvuj", "prvni", "reactivni", "stream")
+				.flatMap(value ->
+								Mono.just(value.toUpperCase())
+										.subscribeOn(Schedulers.parallel()),
+						1)
+
+				.subscribe(value ->	log.info("Consumed: " + value));
+
+		Thread.sleep(2000);
+	}
+
+	@Test
+	public void anotherTest() throws InterruptedException {
+		Flux.just("Ahoj", "Ja", "jsem", "tvuj", "prvni", "reactivni", "stream")
+		        .log()
+				.map(String::toUpperCase)
+				.subscribeOn(Schedulers.newParallel("sub"))
+				.publishOn(Schedulers.newParallel("pub"), 2)
+				.collectList()
+				.subscribe(value -> {
+					log.info("Consumed: " + value);
+				});
+
+		Thread.sleep(2000);
+
+	}
 }
