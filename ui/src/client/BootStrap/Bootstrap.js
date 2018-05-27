5a5c0397ec8d52261502f2aa08ee7433a3d0ea3d
jakub krhovjak <jakub.krhojvak@aevi.cz>
2018-05-27 17:46:45 +0200

    Materialized

--- ui/src/client/BootStrap/Bootstrap.js
+++ ui/src/client/BootStrap/Bootstrap.js
@@ -13,6 +13,8 @@ const styles = {
         height: 48,
         padding: '0 30px',
         boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .30)',
+        textTransform: 'none',
+        
     },
 };
 
@@ -26,7 +28,7 @@ function Bootstrap(props)  {
 
         return (
             <div>
-                <Button className={props.classes.button}><Star/>Button</Button>
+                <Button className={props.classes.button}><Star/>Touch</Button>
             </div>
 
         )
